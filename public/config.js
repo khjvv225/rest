f8882ec24f860a7495c8c2909030236829668c2f
Qurbonali Hojiyev <qurbonalihojiyev@gmail.com>
2026-01-21 18:08:14 +0500

    Premium dizayn va katta ikonlar bilan ro'yxatdan o'tish formasi

--- public/config.js
+++ public/config.js
@@ -1,16 +1,15 @@
 // Firebase konfiguratsiyasi
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD4XwR4KpK9V9L2Q6N7s8P9t0Uv1W2X3Y4Z",
-    authDomain: "bukhara-rest.firebaseapp.com",
-    databaseURL: "https://bukhara-rest-default-rtdb.firebaseio.com",
-    projectId: "bukhara-rest",
-    storageBucket: "bukhara-rest.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef1234567890abcdef"
+    apiKey: "AIzaSyCFDWpwT_oFQV4u4MEjsFDJUNlVx645nVs",
+    authDomain: "bukhara-reest.firebaseapp.com",
+    projectId: "bukhara-reest",
+    storageBucket: "bukhara-reest.appspot.com",
+    messagingSenderId: "51840287890",
+    appId: "1:51840287890:web:952f5fbc4c9ce0627b124b",
+    measurementId: "G-2V3CP2DTZJ"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-const db = firebase.database();
 window.auth = auth;
-window.db = db;
 window.firebase = firebase;
